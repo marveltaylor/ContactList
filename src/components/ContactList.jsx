94bda5a12b75a9f1499314e28962ac8cdad2de7a
marveltaylor <marveltaylor85@gmail.com>
2023-06-20 15:32:10 -0700

    imported ContactRow jsx to ContactList jsx

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import './components/ContactRow.jsx'
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
