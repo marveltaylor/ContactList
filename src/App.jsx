03fbfc1b1cd27cd5429535c683d3e2a0be235606
marveltaylor <marveltaylor85@gmail.com>
2023-06-20 16:54:09 -0700

    created state variables for selectedContactId and setSelectedContactId

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import ContactList from './components/ContactList.jsx';
 
 export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
