dfee5d59522b549268b0c277077d42806f8400f2
marveltaylor <marveltaylor85@gmail.com>
2023-06-20 17:37:56 -0700

    added pseudocode to ContactList component

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -8,7 +8,7 @@ const dummyContacts = [
     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
   ];
 
-export default function ContactList() {
+export default function ContactList({setSelectedContactId}) { //pass setSelectedContactId function to ContactList component
 const [contacts, setContacts] = useState(dummyContacts);
 
 useEffect(() => {
