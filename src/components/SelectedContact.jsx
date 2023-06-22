5a7a77a10fb2842f544134a94de36ba58dd7d0d7
marveltaylor <marveltaylor85@gmail.com>
2023-06-21 17:44:24 -0700

    added contact, setContact state values and passed useState

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from "react";
 
 export default function SelectedContact({SelectedContact, setSelectedContact}) {
+    const [contact, setContact] = useState(null);
 
     return (
         <>
