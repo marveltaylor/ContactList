f3f70d7d80dbf2caf7d16099b550ef62221c5fdd
marveltaylor <marveltaylor85@gmail.com>
2023-06-21 18:25:56 -0700

    imported SelectedContact to App jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import ContactList from './components/ContactList.jsx';
+import SelectedContact from './components/SelectedContact';
 
 export default function App() {
   const [selectedContactId, setSelectedContactId] = useState(null);
