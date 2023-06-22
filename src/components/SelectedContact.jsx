22765fd8b5721de7d43042afd2b677fdbe091cd3
marveltaylor <marveltaylor85@gmail.com>
2023-06-21 17:59:43 -0700

    defined testFunction function that fetches API URL

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -4,7 +4,17 @@ export default function SelectedContact({SelectedContact, setSelectedContact}) {
     const [contact, setContact] = useState(null);
 
     useEffect(() => {
-
+        async function testFunction() {
+            try {
+                const response = await fetch (`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`
+                );
+                const result = await response.json();
+                setContact(result);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        testFunction();
     }, []);
 
     return (
