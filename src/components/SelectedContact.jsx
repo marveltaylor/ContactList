6ccc1e977f2338799602751987054066b4c10a0b
marveltaylor <marveltaylor85@gmail.com>
2023-06-21 18:01:46 -0700

    console logged contact

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from "react";
 
 export default function SelectedContact({SelectedContact, setSelectedContact}) {
     const [contact, setContact] = useState(null);
+    console.log(contact);
 
     useEffect(() => {
         async function testFunction() {
